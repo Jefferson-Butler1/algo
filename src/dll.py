0cafe4d30c566f9fa5a6f6c750f4b412b9ec6259
Jefferson-Butler1 <jbutler@questforward.org>
2024-09-19 23:34:00 -0700

    finish optional methods

--- src/dll.py
+++ src/dll.py
@@ -4,12 +4,16 @@ class Node:
         self.prev = prev
         self.next = next
 
+    def __repr__(self):
+        return str(self.item)
+
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = Node()
         self.head.next = self.head
         self.head.prev = self.head
-    
+
     def __repr__(self) -> str:
         values = []
         curr = self.head.next
@@ -17,6 +21,7 @@ class DoublyLinkedList:
             values.append(str(curr.item))
             curr = curr.next
         return "<" + ", ".join(values) + ">"
+
     def str_reverse(self):
         values = []
         curr = self.head.prev
@@ -25,34 +30,33 @@ class DoublyLinkedList:
             curr = curr.prev
         return "<" + ", ".join(values) + ">"
 
-
     def add_back(self, val):
-        new = Node(item = val, prev = self.head.prev, next = self.head)
+        new = Node(item=val, prev=self.head.prev, next=self.head)
         if not (self.head.prev and new.next):
-            # impossible case, init sets new nodes as self referencing 
+            # impossible case, init sets new nodes as self referencing
             return None
-        self.head.prev= new
+        self.head.prev = new
         new.prev.next = new
 
     def add_front(self, val):
-        new = Node(item = val, prev = self.head, next = self.head.next)
+        new = Node(item=val, prev=self.head, next=self.head.next)
         if not (self.head.next and new.next):
-            # impossible case, init sets new nodes as self referencing 
+            # impossible case, init sets new nodes as self referencing
             return None
         self.head.next = new
         new.next.prev = new
 
     def remove_back(self):
         target = self.head.prev
-        if(target == self.head):
+        if target == self.head:
             return None
-        self.head.prev= self.head.prev.prev
+        self.head.prev = self.head.prev.prev
         self.head.prev.next = self.head
         return target.item
 
     def remove_front(self):
         target = self.head.next
-        if(target == self.head):
+        if target == self.head:
             return None
         self.head.next = self.head.next.next
         self.head.next.prev = self.head
@@ -64,5 +68,20 @@ class DoublyLinkedList:
             self.add_back(next)
             next = more.remove_front()
 
+    def reverse(self):
+        curr = self.head
+        curr.prev, curr.next = curr.next, curr.prev  # switch pointers
+        curr = curr.prev  # goes to next
+        while curr != self.head:
+            curr.prev, curr.next = curr.next, curr.prev  # switch pointers
+            curr = curr.prev  # goes to next
 
+    def remove(self, target):
+        curr = self.head.next
+        while curr != self.head:
+            print(target, curr)
+            if curr.item == target:
+                curr.prev.next = curr.next
+                curr.next.prev = curr.prev
+            curr = curr.next
 
